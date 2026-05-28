37a60dbf52753348c9f6ee2b013aa840ade30da0
justinlai2325 <justinlai@sekolahbim.sch.id>
2026-05-28 20:46:43 +0700

    Update 1form.js

--- 1form.js
+++ 1form.js
@@ -7,6 +7,7 @@ function redirectPage() {
   window.location.href = " https://justinlai2325.github.io/1men.html/";
 }
 </script>
+   <button class="buy-btn" onclick="showPopup()">Buy Now</button>
   <script>
         function showPopup() {
             alert("You have bought the item");
