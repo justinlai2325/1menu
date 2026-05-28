137e180419c2421727feb83deb87eb1dc76c6baf
justinlai2325 <justinlai@sekolahbim.sch.id>
2026-05-28 20:43:02 +0700

    Enhance form with purchase confirmation popup
    Added a popup alert for item purchase confirmation.

--- 1form.js
+++ 1form.js
@@ -7,3 +7,8 @@ function redirectPage() {
   window.location.href = " https://justinlai2325.github.io/1men.html/";
 }
 </script>
+  <script>
+        function showPopup() {
+            alert("You have bought the item");
+        }
+    </script>
